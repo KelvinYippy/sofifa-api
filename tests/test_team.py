bbd374f3615da1ea1ef25ed50c6424d8751f85b2
Kelvin Yip <kelvinyip7@Kelvins-MBP.wireless.yale.internal>
2023-08-28 10:21:41 -0400

    add team lineup tests

--- tests/test_team.py
+++ tests/test_team.py
@@ -1,5 +1,4 @@
 from tests.client import create_client
-from fastapi import Response
 
 client = create_client()
 
@@ -29,3 +28,36 @@ def test_get_national_teams():
     assert first_team["country"] == "Rest of World"
     assert first_team["league"] == "Friendly International"
     assert first_team["squad_size"] == 23
+
+def test_get_starting_lineup():
+    response = client.get("/teams/1/starting_lineup")
+    assert response.status_code == 200
+    assert len(response.json()) == 11
+    json = response.json()[1]
+    assert json["name"] == "B. White"
+    assert json["age"] == 24
+    assert json["overall"] == 80
+    assert json["potential"] == 84
+    assert json["positions"] == ["RB", "CB"]
+
+def test_get_subs():
+    response = client.get("/teams/1/substitutes")
+    assert response.status_code == 200
+    assert len(response.json()) == 7
+    json = response.json()[0]
+    assert json["age"] == 27
+    assert json["name"] == "L. Trossard"
+    assert json["overall"] == 80
+    assert json["positions"] == ["CF", "LM"]
+    assert json["potential"] == 80
+
+def test_get_reserves():
+    response = client.get("/teams/1/reserves")
+    assert response.status_code == 200
+    assert len(response.json()) == 14
+    json = response.json()[1]
+    assert json["age"] == 21
+    assert json["name"] == "E. Smith Rowe"
+    assert json["overall"] == 79
+    assert json["potential"] == 86
+    assert json["positions"] == ["LW", "CAM"]
